fe73dad398c5910cc3800b1156f3a1ecfde11da8
jhdcruz <jhdcruz00@gmail.com>
2020-10-14 18:13:04 +0800

    darkened titlebar color

--- public/preload.js
+++ public/preload.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if (process.platform !== 'darwin') {
     const customTitlebar = require('custom-electron-titlebar');
     new customTitlebar.Titlebar({
-      backgroundColor: customTitlebar.Color.fromHex('#2c2c2c'),
+      backgroundColor: customTitlebar.Color.fromHex('#222'),
       icon: 'favicon.ico',
       titleHorizontalAlignment: 'left',
       menu: null,
