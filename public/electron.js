ccc302604925b19509b4288b6ebff4a8119a8918
jhdcruz <jhdcruz00@gmail.com>
2020-10-14 14:45:44 +0800

    Sentry Integration

--- public/electron.js
+++ public/electron.js
@@ -16,11 +16,13 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
-const isDev = require("electron-is-dev");
+const isDev = require('electron-is-dev');
+const { SentryClient } = require('@sentry/electron');
 
-require('@treverix/remote/main').initialize()
+require('dotenv').config();
+require('@treverix/remote/main').initialize();
 
 function createWindow() {
   // Create the browser window.
@@ -34,40 +36,44 @@ function createWindow() {
       nodeIntegration: true,
       enableRemoteModule: true,
       preload: path.join(__dirname, 'preload.js'),
-    }
-  })
+    },
+  });
 
   // and load the index.html of the app.
   win.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
   );
 
   if (isDev) {
-    win.webContents.openDevTools({mode: 'detach'});
+    win.webContents.openDevTools({ mode: 'detach' });
   }
-
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
+
+// Sentry Integration
+SentryClient.init({
+  dsn: process.env.SENTRY_DSN,
+});
