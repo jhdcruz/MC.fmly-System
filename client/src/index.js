42f0954117545caad64a945d62666c477e822d67
jhdcruz <jhdcruz00@gmail.com>
2020-10-23 23:51:17 +0800

    Revert to `Sentry` Monitoring

--- client/src/index.js
+++ client/src/index.js
@@ -18,12 +18,24 @@
 
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config();
+
+Sentry.init({
+  dsn: `${process.env.SENTRY_URL}`,
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 0.7
+});
+
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={'An error has occured, restart the app!'}>
+      <App />
+    </Sentry.ErrorBoundary>
   </StrictMode>,
   document.getElementById('root')
 );
