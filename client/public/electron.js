3c6ffdfd83dd112662375e8c153ef28d55d97835
jhdcruz <jhdcruz00@gmail.com>
2020-10-28 16:54:36 +0800

    Fixed sudden electron whitescreen

--- client/public/electron.js
+++ client/public/electron.js
@@ -49,6 +49,10 @@ function createWindow() {
   if (isDev) {
     win.webContents.openDevTools({ mode: 'detach' });
   }
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 // This method will be called when Electron has finished
