dc68d564ace212bba61ed1a5ff52c16319b6cdcc
jhdcruz <jhdcruz00@gmail.com>
2020-10-30 16:44:24 +0800

    Fixed multiple scrollbars on `table`

--- client/src/components/ExtendedTable.js
+++ client/src/components/ExtendedTable.js
@@ -23,16 +23,18 @@ import styled, { createGlobalStyle } from 'styled-components';
 
 const TableContainer = createGlobalStyle`
   div.table-responsive {
-    display: contents !important;
-    width: 100%;
+    display: inline-flex !important;
+    width: 100vw;
+    height: 100%;
     padding: 0 0 0 0.5rem !important;
     margin: 0;
-    overflow: auto !important;
+    overflow: hidden !important;
 
     table {
       width: 100vw !important;
       padding: 0 3rem 0 1rem;
-      margin: 3rem 0 0 0 !important;
+      margin: 3rem 1rem 2rem 0 !important;
+      overflow: auto !important
     }
 
     #ItemCode {
@@ -42,6 +44,7 @@ const TableContainer = createGlobalStyle`
 `;
 
 const TableHeader = styled.th`
+  font-size: 1.3vw;
   color: #c3c3c3;
   border: none !important;
   width: max-content !important;
@@ -54,10 +57,10 @@ const TableHeader = styled.th`
 
 const ProductTable = styled(Table)`
   display: inline-table;
-  height: 100%;
+  height: 100vh;
   width: 100%;
   min-width: 100% !important;
-  min-height: 100%;
+  min-height: 100vh;
   margin: 0 0 auto 0;
   overflow: auto !important;
   background-color: transparent;
