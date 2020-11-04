c99a3bbca20ef99d4fd45dd4e9a8425599df03f1
jhdcruz <jhdcruz00@gmail.com>
2020-11-05 00:18:38 +0800

    Updated copyright notices

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,3 @@
-/*
- *     MC.fmly Inventory System
- *     Copyright (C) 2020  Joshua Hero H. Dela Cruz
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
