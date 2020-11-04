68ae5badfa7d3ef72af99d013e88bc633458ffd4
jhdcruz <jhdcruz00@gmail.com>
2020-11-04 23:43:47 +0800

    Fixed duplicated `PieChart`
    Remove duplicate piechart

--- src/components/charts/ProductsAllocation.js
+++ src/components/charts/ProductsAllocation.js
@@ -1,73 +1,30 @@
-/*
- *     MC.fmly Inventory System
- *     Copyright (C) 2020  Joshua Hero H. Dela Cruz
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 import { PureComponent } from 'react';
-import { Cell, Pie, PieChart } from 'recharts';
-import data from './json/pieChart.json';
-
-const COLORS = ['#22a1f5', '#4de670', '#faa142', '#FF8042'];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-  index
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? 'start' : 'end'}
-      dominantBaseline="central"
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
+import { Pie, PieChart, Tooltip } from 'recharts';
+import data from './json/productsCount.json';
 
 export default class ProductsAllocation extends PureComponent {
   render() {
     return (
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          cx={200}
-          cy={200}
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={100}
-          fill="#232323"
-          dataKey="value"
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
+      <div
+        style={{
+          width: '100%',
+          height: 300
+        }}
+      >
+        <PieChart width={550} height={400}>
+          <Pie
+            dataKey="value"
+            isAnimationActive={false}
+            data={data}
+            cx={200}
+            cy={200}
+            outerRadius={80}
+            fill="#8884d8"
+            label
+          />
+          <Tooltip />
+        </PieChart>
+      </div>
     );
   }
 }
