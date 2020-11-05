be0112add6793a5abf70c74c877a82d12c070609
jhdcruz <jhdcruz00@gmail.com>
2020-11-06 00:41:27 +0800

    Updated `ProductDetails` charts style

---
+++ src/components/charts/ProductDetails.js
@@ -0,0 +1,88 @@
+/*
+ *     MC.fmly Inventory System
+ *     Copyright (C) 2020  Joshua Hero H. Dela Cruz
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { PureComponent } from 'react';
+import { Cell, Pie, PieChart, Tooltip } from 'recharts';
+import data from './json/productDetails.json';
+
+const COLORS = ['#2579f6', '#4de670', '#faa142', '#FF8042', '#e93ff5'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+export default class ProductDetails extends PureComponent {
+  render() {
+    return (
+      <>
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx={200}
+            cy={200}
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip itemStyle={{
+            color: 'whitesmoke'
+          }} />
+        </PieChart>
+        <ul>
+          {data.map((entry, index) => (
+            <li key={`cell-${index}`} backgroundColor={COLORS[index % COLORS.length]}>
+              {entry.name}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
