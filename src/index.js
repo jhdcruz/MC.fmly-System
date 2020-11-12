9fa3cfb8a3eea082a1d7b63bb237dfd14840293f
jhdcruz <jhdcruz00@gmail.com>
2020-11-12 14:43:58 +0800

    removed serviceWorker import | sonarlint
    wrong commit earlier

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
