22a2bdc7913bd593fdf1eead84548256c7d0f0ee
jhdcruz <jhdcruz00@gmail.com>
2020-11-15 16:15:06 +0800

    Redirect `cashier` role to `POS` on login

--- src/views/Cashier.js
+++ src/views/Cashier.js
@@ -17,72 +17,13 @@
  */
 
 import { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { AnimatedSwitch, spring } from 'react-router-transition';
-import Container from 'react-bootstrap/Container';
-import Sidebar from '../components/Sidebar';
-import routes from '../routes.js';
 import '../App.scss';
-// Routes
 import PointOfSale from '../containers/PointOfSale';
-import Settings from '../containers/Settings';
-
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`
-  };
-}
-
-// ? wrap the `spring` helper to use a bouncy config
-function bounce(val) {
-  return spring(val, {
-    stiffness: 200,
-    damping: 30
-  });
-}
-
-const bounceTransition = {
-  // ? tart in a transparent, upscaled state
-  atEnter: {
-    opacity: 0,
-    scale: 0.95
-  },
-  // ? leave in a transparent, downscaled state
-  atLeave: {
-    opacity: bounce(0),
-    scale: bounce(0.9)
-  },
-  // ? and rest at an opaque, normally-scaled state
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1)
-  }
-};
 
 export default function Admin() {
   return (
     <Fragment>
-      <Router>
-        <Container fluid>
-          <Sidebar />
-          <div id="pageRoutes" className="routerContainer">
-            <Switch>
-              {/* Router Transition */}`
-              <AnimatedSwitch
-                atEnter={bounceTransition.atEnter}
-                atLeave={bounceTransition.atLeave}
-                atActive={bounceTransition.atActive}
-                mapStyles={mapStyles}
-                className="routerContent"
-              >
-                <Route path={routes.POS} component={PointOfSale} />
-                <Route path={routes.SETTINGS} component={Settings} />
-              </AnimatedSwitch>
-            </Switch>
-          </div>
-        </Container>
-      </Router>
+      <PointOfSale />
     </Fragment>
   );
 }
