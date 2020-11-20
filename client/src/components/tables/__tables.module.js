3a4f977a710a8eb6d0e65d66a04a943846195625
jhdcruz <jhdcruz00@gmail.com>
2020-11-21 00:57:38 +0800

    Added `box-shadow` for tags

--- client/src/components/tables/__tables.module.js
+++ client/src/components/tables/__tables.module.js
@@ -47,6 +47,7 @@ export const Loader = styled(Spinner)`
 export const Tag = styled(Badge)`
   font-size: 1.2vw;
   padding: 6px 10px 6px 0;
+  box-shadow: 0 1px 5px 1px #1e1e1e;
 `;
 
 // Shows on no products registered
