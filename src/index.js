e842d8ae96160d598a4f0c4c3a850177ab849bd4
jhdcruz <jhdcruz00@gmail.com>
2020-11-21 21:12:16 +0800

    HOTFIX: Fixed Login Authentication API

--- src/index.js
+++ src/index.js
@@ -20,8 +20,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/tracing';
-// import App from './App';
-import Admin from './views/Admin';
+import App from './App';
 
 Sentry.init({
   dsn:
@@ -35,7 +34,7 @@ Sentry.init({
 
 ReactDOM.render(
   <StrictMode>
-    <Admin />
+    <App />
   </StrictMode>,
   document.getElementById('root')
 );
