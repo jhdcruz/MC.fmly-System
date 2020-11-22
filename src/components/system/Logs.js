2188a86fc5ef02abc4ac340394619db6075a989d
jhdcruz <jhdcruz00@gmail.com>
2020-11-22 18:59:24 +0800

    Added `Logs` system info sample template

---
+++ src/components/system/Logs.js
@@ -0,0 +1,50 @@
+/*
+ *     MC.fmly Inventory System
+ *     Copyright (C) 2020  Joshua Hero H. Dela Cruz
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export default function Logs() {
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh'
+      }}
+    >
+      <blockquote
+        className="logdna-embed"
+        data-embed-id="44a57a0868"
+        data-embed-org="f5de57b7a2"
+        data-embed-signature="ff4f2f517c8a57699770aa081a87be42f6b82604e07fd6a15988a3d8c4a5da227d7e4ace864395f0cebfd1a425192a457a6f47097c88eef410ea80c3211dcd3a"
+      >
+        <h4>
+          <a
+            href="https://app.logdna.com/ext/embed-link/f5de57b7a2/44a57a0868"
+            target="return func(0)"
+          >
+            System Logs
+          </a>
+        </h4>
+        <footer>
+          <a href="https://logdna.com" target="return func(0)">
+            by LogDNA
+          </a>
+        </footer>
+      </blockquote>
+      <script async defer src="https://cdn.logdna.net/js/embed.js" />
+    </div>
+  );
+}
