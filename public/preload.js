a28ed446bc99085c02ef63112620682b31ff1e37
jhdcruz <jhdcruz00@gmail.com>
2020-11-22 13:07:56 +0800

    Removed built-in `Menu` shell

--- public/preload.js
+++ public/preload.js
@@ -20,11 +20,10 @@ window.addEventListener('DOMContentLoaded', () => {
   if (process.platform !== 'darwin') {
     const customTitlebar = require('custom-electron-titlebar');
     new customTitlebar.Titlebar({
-      backgroundColor: customTitlebar.Color.fromHex('#1e1e1e'),
+      backgroundColor: customTitlebar.Color.fromHex('#222126'),
       icon: 'favicon.ico',
-      titleHorizontalAlignment: 'center',
-      menuPosition: 'left',
-      enableMnemonics: 'true'
+      titleHorizontalAlignment: 'left',
+      menu: null
     });
   }
 });
