6447103bf4c1ee50254d510d379f5ff69e437fe8
jhdcruz <jhdcruz00@gmail.com>
2020-11-25 20:21:05 +0800

    Updated `key` props for mapping

--- client/src/containers/AuditLog.js
+++ client/src/containers/AuditLog.js
@@ -30,7 +30,7 @@ export default function AuditLog() {
   const ListProducts = () => {
     return (
       <ProductHeader
-        map={products && products._id}
+        _id={products && products._id}
         data={
           products && products.length !== null ? (
             // Reverse & limit result to 20 | prioritize new entries
