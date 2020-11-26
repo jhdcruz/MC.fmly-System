05e9fa5e8b02353d555826931a559c6664e9e875
jhdcruz <jhdcruz00@gmail.com>
2020-11-26 21:54:45 +0800

    Match titlebar color to theme

--- client/public/preload.js
+++ client/public/preload.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', () => {
   if (process.platform !== 'darwin') {
     const customTitlebar = require('custom-electron-titlebar');
     new customTitlebar.Titlebar({
-      backgroundColor: customTitlebar.Color.fromHex('#19191c'),
+      backgroundColor: customTitlebar.Color.fromHex('#222126'),
       icon: 'favicon.ico',
       titleHorizontalAlignment: 'left',
       menu: null
