46d86e99b5ef5f6ab38dfa6b7679b70ec0c38db1
jhdcruz <jhdcruz00@gmail.com>
2020-11-29 21:00:31 +0800

    Load LogDNA embed immediately

--- client/src/components/system/Logs.js
+++ client/src/components/system/Logs.js
@@ -17,6 +17,7 @@
  */
 
 import styled from 'styled-components';
+import Loader from '../common/Loader';
 
 /*****************************************************
  * * Embed API Logs (LogDNA)
@@ -41,14 +42,14 @@ export default function Logs() {
         data-embed-id="d126b062b1"
         data-embed-org="f5de57b7a2"
         data-embed-signature="d8c4fecd3cc97523a26e58ae11eb1a32752adb9d7b7393fa4257153c608acee9800226681653f864ab20c28ced34adf0bff737217dad8ecc86cb0f7a65082e5c"
+        rel="prefetch"
       >
         <h4>
           <a
             href="https://app.logdna.com/ext/embed-link/f5de57b7a2/d126b062b1"
-            target="_blank"
-            rel="noreferrer"
+            rel="prefetch"
           >
-            MC.fmly Inventory System
+            <Loader />
           </a>
         </h4>
       </blockquote>
