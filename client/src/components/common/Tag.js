a26b8300858d033f2b506b6dc40db588f7837657
jhdcruz <jhdcruz00@gmail.com>
2020-12-01 17:16:12 +0800

    Removed Tags custom padding

--- client/src/components/common/Tag.js
+++ client/src/components/common/Tag.js
@@ -22,7 +22,6 @@ import Badge from 'react-bootstrap/Badge';
 // * Color Badges
 const Chip = styled(Badge)`
   font-size: 1.2vw;
-  padding: 6px 10px 6px 0;
   border-radius: 0.3rem;
   box-shadow: 1px 2px 5px #1b1b1b;
 `;
