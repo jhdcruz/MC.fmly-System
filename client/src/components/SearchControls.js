0dc821dfdad29443f304bc6fa7c584bb4b8c6e6c
jhdcruz <jhdcruz00@gmail.com>
2020-12-04 17:05:02 +0800

    Updated Controls required attr.

--- client/src/components/SearchControls.js
+++ client/src/components/SearchControls.js
@@ -17,39 +17,34 @@
  */
 
 import styled from 'styled-components';
-import SearchBar from './common/SearchBar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faPlus, faThLarge } from '@fortawesome/free-solid-svg-icons';
 import { Control } from './Control';
+import SearchBar from './common/SearchBar';
+import { faBars, faPlus, faThLarge } from '@fortawesome/free-solid-svg-icons';
 
 /**************************************
- * * Searchbar w/ Add & View Controls
+ * * Searchbar w/ Default Controls
  **************************************/
 
 const ControlSection = styled.div`
   display: inline-flex;
   width: 92vw;
   height: max-content;
-  margin: 0.3rem 1rem 0;
-  padding: 0 0.5rem;
+  margin: 0 1rem 0;
+  padding: 0.3rem 0.5rem;
   overflow: hidden;
 `;
 
-export default function SearchControls({ modal }) {
+export default function SearchControls(props) {
   return (
     <ControlSection>
       <SearchBar />
-      <Control onClick={modal}>
-        <FontAwesomeIcon icon={faPlus} />
-      </Control>
+
+      {/* Add Entry Modal */}
+      <Control tooltip={props.add} action={props.modal} icon={faPlus} />
 
       {/* Views Types */}
-      <Control>
-        <FontAwesomeIcon icon={faBars} />
-      </Control>
-      <Control>
-        <FontAwesomeIcon icon={faThLarge} />
-      </Control>
+      <Control tooltip={props.list} action={props.listView} icon={faBars} />
+      <Control tooltip={props.card} action={props.cardView} icon={faThLarge} />
     </ControlSection>
   );
 }
