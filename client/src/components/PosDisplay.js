d346471a96d528170f64f8af4da486ab5b63b329
jhdcruz <jhdcruz00@gmail.com>
2020-12-04 23:26:50 +0800

    Updated copyright headers

--- client/src/components/PosDisplay.js
+++ client/src/components/PosDisplay.js
@@ -1,19 +1,6 @@
 /*
  *     MC.fmly Inventory Management System
- *     Copyright (C) 2020  Joshua Hero H. Dela Cruz
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     Copyright (C) 2020  Joshua Hero Dela Cruz
  */
 
 import styled from 'styled-components';
@@ -111,7 +98,6 @@ export default function PosDisplay(props) {
             <h4>Items: {props.items || 0}</h4>
           </Col>
           <Col>
-            {/* VAT Input */}
             <InputGroup className="mb-3">
               <InputGroup.Prepend>
                 <InputGroup.Text>VAT:</InputGroup.Text>
@@ -121,8 +107,6 @@ export default function PosDisplay(props) {
                 <InputGroup.Text>%</InputGroup.Text>
               </InputGroup.Append>
             </InputGroup>
-
-            {/* Total * VAT % */}
             <h4>Total Amount: ₱{props.total || '0.00'}</h4>
           </Col>
         </Row>
