eafc1796a14ca53ed75071bddf3f0e81d2202189
jhdcruz <jhdcruz00@gmail.com>
2020-12-05 18:43:05 +0800

    Updated copyright headers

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,6 +1,7 @@
 /*
  *     MC.fmly Inventory Management System
  *     Copyright (C) 2020  Joshua Hero Dela Cruz
+ *     Licensed under GNU General Public License 3.0 or later
  */
 
 const ProductController = require('../controllers/product.controller');
