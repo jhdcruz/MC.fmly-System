0c81d6eb97978f88d4b16a62f410a9644666f6ea
jhdcruz <jhdcruz00@gmail.com>
2020-12-08 16:06:21 +0800

    Renamed Control to Controls

--- client/src/components/PosDisplay.js
+++ client/src/components/PosDisplay.js
@@ -9,7 +9,7 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
-import { Control, LargeControl } from './common/Control';
+import { Control, LargeControl } from './common/Controls';
 import { faShoppingBasket, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const PosContainer = styled.div`
