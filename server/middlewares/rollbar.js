dee267416f9e17bd041083ead0691b8d28efb74b
jhdcruz <jhdcruz00@gmail.com>
2020-12-17 11:55:27 +0800

    Updated copyright headers

--- server/middlewares/rollbar.js
+++ server/middlewares/rollbar.js
@@ -1,3 +1,9 @@
+/*
+ *     MC.fmly Inventory Management System
+ *     Copyright (C) 2020  Joshua Hero Dela Cruz
+ *     Licensed under GNU General Public License 3.0 or later
+ */
+
 const Rollbar = require('rollbar');
 
 // * Rollbar config
