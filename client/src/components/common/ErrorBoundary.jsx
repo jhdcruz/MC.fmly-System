d2f2d82a9700fa26c97889c381414a91b02d8387
jhdcruz <jhdcruz00@gmail.com>
2020-12-19 01:15:51 +0800

    Added error boundary fallback UI

---
+++ client/src/components/common/ErrorBoundary.jsx
@@ -0,0 +1,29 @@
+/*
+ *     MC.fmly Inventory Management System
+ *     Copyright (C) 2020  Joshua Hero Dela Cruz
+ *     Licensed under GNU General Public License 3.0 or later
+ */
+
+import { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    // TODO: Update UI
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
