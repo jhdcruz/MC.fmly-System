eb9199979e43c0b3e01e38514d37c30445ca2523
jhdcruz <jhdcruz00@gmail.com>
2021-01-07 19:04:52 +0800

    REVERT: Patch login api route
    This reverts commit 6d5366f9eb6960b4209b71ddfbed60a3017a3793.

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -7,6 +7,6 @@
 const AuthController = require('../controllers/auth.controller');
 
 module.exports = (api) => {
-  api.get('/auth/status', AuthController.status);
+  api.get('/api/status', AuthController.status);
   api.post('/auth/login', AuthController.login);
 };
