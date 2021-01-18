67678dc2bae50008928c99765e81d290b815173a
jhdcruz <jhdcruz00@gmail.com>
2021-01-18 18:09:36 +0800

    UI/UIX(client): Maintenance notice

--- client/src/components/common/Maintenance.jsx
+++ client/src/components/common/Maintenance.jsx
@@ -6,24 +6,39 @@
 
 import { memo } from 'react';
 import styled from 'styled-components';
+import Image from 'react-bootstrap/Image';
+import logo from '../../assets/img/logo.svg';
 
 /************************
  * * Maintenance Notice
  ************************/
 
 const Notice = styled.h2`
-  color: #e6a195;
-  background-color: rgba(35, 34, 36, 0.9);
-  padding: 3rem;
-  border-radius: 0.7rem;
-  margin: 5rem auto;
-  z-index: 99;
+  width: max-content;
+  text-align: center;
+  margin: 0 auto;
+  color: whitesmoke;
+  background-color: #222126;
+  box-shadow: 3px 3px 8px #1b1b1b;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+
+  #feature {
+    color: #e6a195;
+    line-height: 1;
+  }
 `;
 
-const Maintenance = memo(() => {
+const Img = styled(Image)`
+  margin: 0.5rem 0;
+`;
+
+const Maintenance = memo(({ feature }) => {
   return (
     <Notice>
-      <strong>This feature is currently under maintenance</strong>
+      <Img src={logo} width={200} height={200} />
+      <h3 id="feature">{feature}</h3>
+      <h5>is currently undergoing maintenance</h5>
     </Notice>
   );
 });
